881aad8a4f72da30ea341c9b13a2001129290180
kmkkkp <gimmingyu@gimmingyudeMacBook-Pro.local>
2024-04-16 17:13:43 +0900

    DEL env

--- am_app/lib/screen/map/search_service.dart
+++ am_app/lib/screen/map/search_service.dart
@@ -1,12 +1,10 @@
 import 'package:google_maps_webservice/places.dart';
 
 class SearchService {
-  final places = GoogleMapsPlaces(apiKey: 'AIzaSyD14W6fylFGmSu4OKau1irIcgdaP-utQzo');
+  final places = GoogleMapsPlaces(apiKey: "");
 
   Future<List<PlacesSearchResult>> searchPlaces(String query) async {
     PlacesSearchResponse response = await places.searchByText(query);
     return response.results;
   }
-
-  
 }
