d2a1f881babe628e2652b7a48d9eb4305f0fffaa
kmkkkp <gimmingyu@gimmingyudeMacBook-Pro.local>
2024-04-21 20:44:14 +0900

    UPDATE apikey

--- am_app/lib/screen/map/search_service.dart
+++ am_app/lib/screen/map/search_service.dart
@@ -1,7 +1,9 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_maps_webservice/places.dart';
 
 class SearchService {
-  final places = GoogleMapsPlaces(apiKey: "");
+  late final apiKey = dotenv.env['GOOGLE_MAPS_API_KEY'];
+  late final places = GoogleMapsPlaces(apiKey: apiKey);
 
   Future<List<PlacesSearchResult>> searchPlaces(String query) async {
     PlacesSearchResponse response = await places.searchByText(query);
